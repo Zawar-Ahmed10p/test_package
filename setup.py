762de6ec1d02c9ac8f6bf6a375cdf26407a92b58
Zawar-Ahmed10p <56343465+Zawar-Ahmed10p@users.noreply.github.com>
2019-11-15 10:38:34 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='test_package',
-    version='0.0.1',
+    version='0.0.2',
     description='My private package from private github repo',
     url='https://github.com/Zawar-Ahmed10p/test-package.git',
     author='Zawar',
-    author_email='zawar.ahmed@tenpearls.com',
+    //author_email='zawar.ahmed@tenpearls.com',
     license='unlicense',
     packages=['test_package'],
     zip_safe=False
