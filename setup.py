0ac40fd115015a788b60ab391a517f1f05835562
faizanahmed-10p <faizan.ahmed@tenpearls.com>
2019-11-15 10:21:43 +0500

    name changed

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='test-package',
+    name='test_package',
     version='0.0.1',
     description='My private package from private github repo',
     url='https://github.com/Zawar-Ahmed10p/test-package.git',
     author='Raphael Schubert',
     author_email='zawar.ahmed@tenpearls.com',
     license='unlicense',
-    packages=['test-package'],
+    packages=['test_package'],
     zip_safe=False
 )
